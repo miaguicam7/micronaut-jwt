0d05fb425a506fdcbc916cb232bde68374950a5a
Miguel Ángel Aguilar <miaguicam@gmail.com>
2019-04-16 11:52:29 +0200

    signup feature

--- src/main/kotlin/example/micronaut/Controller/UserController.kt
+++ src/main/kotlin/example/micronaut/Controller/UserController.kt
@@ -2,16 +2,33 @@ package example.micronaut.Controller
 
 import example.micronaut.model.User
 import example.micronaut.repository.UserRepository
+import io.micronaut.http.HttpResponse
 import io.micronaut.http.MediaType
+import io.micronaut.http.annotation.Body
 import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Get
-import reactor.core.publisher.Flux
+import io.micronaut.http.annotation.Post
+import javax.validation.Valid
 
 @Controller("/users")
-class UserController(val userRepository: UserRepository) {//(private val userRepository: UserRepository){
+class UserController(val userRepository: UserRepository) {
 
-    @Get(produces = [MediaType.TEXT_EVENT_STREAM])
-    fun getAllUsers()= Flux.just(User("Aurelio", "abmf", "vaca"),
-            User("Flavio", "ffpsj", "boi"), User("Malta", "malta", "mm"),
-            User("Malta", "malta", "mm"), User("Pedro", "ps", "1234"))
+    @Get(produces = [MediaType.APPLICATION_JSON])
+    fun getAllUsers(): HttpResponse<*> {
+        return HttpResponse.ok<Any>(userRepository.getAllUsers())
+    }
+
+    //curl --request POST -H "Content-Type: application/json" --data '{"name":"Malta","login":"malta","password":"mm"}' http://localhost:8080/users
+    @Post(produces = [MediaType.APPLICATION_JSON])
+    fun signIn(@Body @Valid user: User): HttpResponse<*> {
+        return HttpResponse.ok<Any>(userRepository.findUserByLoginAndPassword(user))
+    }
+
+
+    //curl --request POST -H "Content-Type: application/json" --data '{"name":"Malta","login":"malta","password":"mm"}' http://localhost:8080/users/signup
+    @Post("/signup", produces = [MediaType.APPLICATION_JSON])
+    fun singUp(@Body @Valid user: User): HttpResponse<*> {
+        val savedUser = userRepository.findUserByLoginAndPassword(user)?.takeIf { it == null }.let { userRepository.addUser(user) }
+        return if (savedUser) HttpResponse.created<Any>(user) else HttpResponse.badRequest<Any>()
+    }
 }
--- src/main/kotlin/example/micronaut/repository/UserRepository.kt
+++ src/main/kotlin/example/micronaut/repository/UserRepository.kt
@@ -1,26 +1,26 @@
 package example.micronaut.repository
 
 import example.micronaut.model.User
-import reactor.core.publisher.Flux
 import javax.inject.Singleton
 
 @Singleton
 class UserRepository {
+    private var users = ArrayList<User>()
 
-    fun getAllUsers(): Flux<Any> = Flux.just(User("Aurelio", "abmf", "vaca"),
-            User("Flavio", "ffpsj", "boi"),User("Malta", "malta", "mm"),
-            User("Malta", "malta", "mm"),User("Pedro", "ps", "1234"))
+    init{
+        users.add(User("Aurelio", "abmf", "vaca"))
+        users.add(User("Flavio", "ffpsj", "boi"))
+        users.add(User("Malta", "malta", "mm"))
+        users.add(User("Pedro", "ps", "1234"))
+    }
 
-/*
     fun getAllUsers() = users
 
     fun removeUserByLogin(user: User) = users.removeAll { u -> u.login ==  user.login}
 
-    fun addUser(user: User) = users.add(user)
+    fun addUser(user: User)= users.add(user)
 
     fun findUserByLoginAndPassword(user: User) = users.find { u ->
         u.login == user.login && u.password == user.password
     }
-    */
-
 }
