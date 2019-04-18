672d2405654853aaf87c980c70a41b625bbda762
Miguel Ángel Aguilar <miaguicam@gmail.com>
2019-04-18 13:20:58 +0200

    secure tests

--- src/main/kotlin/example/micronaut/controller/UserController.kt
+++ src/main/kotlin/example/micronaut/controller/UserController.kt
@@ -6,29 +6,28 @@ import io.micronaut.http.HttpResponse
 import io.micronaut.http.MediaType
 import io.micronaut.http.annotation.*
 import io.micronaut.security.annotation.Secured
+import io.micronaut.security.rules.SecurityRule
 import javax.validation.Valid
 
 
 @Controller("/users")
 class UserController(val userRepository: UserRepository) {
 
-    @Secured("isAuthenticated()")
+    @Secured(SecurityRule.IS_AUTHENTICATED)
     @Get(produces = [MediaType.APPLICATION_JSON])
     fun list() = HttpResponse.ok<Any>(userRepository.getAllUsers())
 
+    @Secured(SecurityRule.IS_ANONYMOUS)
     @Post(produces = [MediaType.APPLICATION_JSON])
     fun signIn(@Body @Valid user: User): HttpResponse<*>  {
         val rsp = userRepository.findUserByLoginAndPassword(user)
         return if (rsp !=null) HttpResponse.ok<Any>(rsp) else HttpResponse.noContent<Any>()
     }
 
-
+    @Secured(SecurityRule.IS_ANONYMOUS)
     @Post("/signup", produces = [MediaType.APPLICATION_JSON])
     fun singUp(@Body @Valid user: User): HttpResponse<*> {
         val rsp = userRepository.findUserByLoginAndPassword(user)?.takeIf { it == null }.let { userRepository.addUser(user) }
         return if (rsp) HttpResponse.created<Any>(user) else HttpResponse.badRequest<Any>()
     }
-
-    @Get("/secret", produces = [MediaType.APPLICATION_JSON])
-    fun getSecret() = "You're accessing a protected endpoint"
 }
--- src/test/kotlin/example/micronaut/controller/UserControllerTest.kt
+++ src/test/kotlin/example/micronaut/controller/UserControllerTest.kt
@@ -2,16 +2,19 @@ package example.micronaut.controller
 
 import example.micronaut.model.User
 import io.micronaut.context.ApplicationContext
+import io.micronaut.http.HttpHeaders
 import io.micronaut.http.HttpRequest
 import io.micronaut.http.HttpResponse
 import io.micronaut.http.HttpStatus
 import io.micronaut.http.client.HttpClient
 import io.micronaut.runtime.server.EmbeddedServer
+import io.micronaut.security.authentication.UsernamePasswordCredentials
+import io.micronaut.security.token.jwt.render.BearerAccessRefreshToken
+
 import org.jetbrains.spek.api.Spek
 import org.jetbrains.spek.api.dsl.describe
 import org.jetbrains.spek.api.dsl.it
-import org.junit.Assert.assertEquals
-import org.junit.Assert.assertFalse
+import org.junit.Assert.*
 
 object UserControllerTest : Spek({
 
@@ -19,23 +22,39 @@ object UserControllerTest : Spek({
         val embeddedServer : EmbeddedServer = ApplicationContext.run(EmbeddedServer::class.java)
         val client : HttpClient = HttpClient.create(embeddedServer.url)
 
+        val users = "/users"
+        var accessToken : String? = null
+
+        beforeGroup {
+            val creds = UsernamePasswordCredentials("sherlock", "password")
+            val request = HttpRequest.POST("/login", creds)
+
+            val response: HttpResponse<BearerAccessRefreshToken> = client.toBlocking().exchange(request,
+                    BearerAccessRefreshToken::class.java)
+
+            assertEquals(response.status()!!, HttpStatus.OK)
+
+            accessToken = response.body()!!.accessToken
+        }
+
         it("test /users responds list of Users") {
-            val users = client.toBlocking().retrieve("/users")
-            assertFalse(users.isEmpty());
+            val request = HttpRequest.GET<String>("$users").header(HttpHeaders.AUTHORIZATION, "Bearer $accessToken")
+            val response : HttpResponse<Any> = client.toBlocking().exchange(request)
+            assertEquals(response.status, HttpStatus.OK)
         }
 
         it("test /users signIn user ok") {
             val user = User("Malta","malta","mm")
-            val request = HttpRequest.POST("/users", user)
-            val rsp : HttpResponse<Any> = client.toBlocking().exchange(request)
-            assertEquals(rsp.status, HttpStatus.OK)
+            val request = HttpRequest.POST("$users", user)
+            val response : HttpResponse<Any> = client.toBlocking().exchange(request)
+            assertEquals(response.status, HttpStatus.OK)
         }
 
         it("test /users signIn user no content") {
             val user = User("Maltade","maltdedea","mmdede")
-            val request = HttpRequest.POST("/users", user)
-            val rsp : HttpResponse<Any> = client.toBlocking().exchange(request)
-            assertEquals(rsp.status, HttpStatus.NO_CONTENT)
+            val request = HttpRequest.POST("$users", user)
+            val response : HttpResponse<Any> = client.toBlocking().exchange(request)
+            assertEquals(response.status, HttpStatus.NO_CONTENT)
         }
 
         afterGroup {
